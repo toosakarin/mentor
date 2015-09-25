2797fcafc7a1f9e26ab036c49265b8fa18eaf08d
jacktseng <jack.tseng@fuhu.com>
2015-09-25 17:27:39 +0800

    first commit just for test the combine of eclipse + xmapp

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "jack");
+define("DB_PASSWORD", "25082797");
+define("DB_DATABASE", "magic_mentor");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", ""); // Place your Google API Key
+?>
